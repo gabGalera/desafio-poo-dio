67bbb40b79c521a28abf67f035faa8bd55213b1c
Gabriel Galera <gabgalera@hotmail.com>
2023-05-22 12:04:47 -0300

    Mentoria e Curso

---
+++ src/main/java/desafio/dio/Main.java
@@ -0,0 +1,30 @@
+package desafio.dio;
+
+import desafio.dio.dominio.Curso;
+import desafio.dio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descrição curso js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de Java");
+        mentoria.setDescricao("descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+    }
+}
